4248c44a245c4962fb91ac3b730d726dfc2b60bf
Endovitskaya Marina <m.endovitskaya@mail.msk>
2019-10-23 14:15:13 +0300

    style fix

--- test_items.py
+++ test_items.py
@@ -1,9 +1,6 @@
-import math
-
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_basket_button(browser):
-    browser.implicitly_wait(5)
     browser.get(link)
     basket_button = browser.find_elements_by_css_selector(".btn-add-to-basket")
     assert len(basket_button) > 0, "На странице товара не найдена кнопка добавления в корзину"
